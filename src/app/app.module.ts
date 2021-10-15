9a0c9cc89e4241222c43a3ac6a76749169312484
Shailendra Singh <shailendramonu@gmail.com>
2021-10-15 16:36:46 +1100

    Adding video component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideoThumbnailComponent } from './video-thumbnail/video-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoThumbnailComponent
   ],
   imports: [
     BrowserModule
